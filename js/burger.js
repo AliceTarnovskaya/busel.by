0df97dacf8e22a165dd87fdab1060d030895abbb
andrejvanin <andrej.a.vanin@gmail.com>
2020-05-23 10:31:14 +0300

    miniToursPage

--- js/burger.js
+++ js/burger.js
@@ -1,5 +1,14 @@
 'use strict';
 $(document).ready(function () {
+    let firstCard = document.getElementById("first-card");
+    let secondCard = document.getElementById("second-card");
+    let thirdCard = document.getElementById("third-card");
+    let firstguide = document.getElementById("first-guide");
+    let secondguide = document.getElementById("second-guide");
+    let thirdguide = document.getElementById("third-guide");
+    let left = document.getElementById("left-znak");
+    let right = document.getElementById("right-znak");
+
     $(function () {
         $(".burger").click(function () {
             if (number % 2 == 0) {
@@ -20,7 +29,89 @@ $(document).ready(function () {
         });
     });
 
+    $(function () {
+        $(".burger").click(function () {
+            if (number % 2 == 0) {
+                $(".burger__line_third").show();
+                $(".burger__line_first").css('top', '0');
+                $(".burger__line_second").css('bottom', '9px');
+                $(".burger__line_first").css('transform', 'rotate(0)');
+                $(".burger__line_second").css('transform', 'rotate(0)');
+            } else {
 
+                $(".burger__line_third").hide();
+                $(".burger__line_first").css('top', '9px');
+                $(".burger__line_second").css('bottom', '9px');
+                $(".burger__line_first").css('transform', 'rotate(45deg)');
+                $(".burger__line_second").css('transform', 'rotate(-45deg)');
+            }
+
+        });
+    });
+    $(function () {
+        $(firstCard).click(function () {
+            $(firstguide).show(300);
+            $(secondguide).hide(300);
+            $(thirdguide).hide(300);
+
+        });
+        $(secondCard).click(function () {
+            $(secondguide).show(300);
+            $(firstguide).hide(300);
+            $(thirdguide).hide(300);
+            $(secondguide).css('display', 'flex');
+
+        });
+        $(thirdCard).click(function () {
+            $(thirdguide).show(300);
+            $(secondguide).hide(300);
+            $(firstguide).hide(300);
+            $(thirdguide).css('display', 'flex');
+
+        });
+        // $(right).click(function () {
+        //     // znakleft();
+        //     if($(firstguide).css('display') == 'flex'){
+        //         $(secondguide).show(300);
+        //         $(firstguide).hide(300);
+        //     } if($(secondguide).css('display') == 'flex'){
+        //         $(thirdguide).show(300);
+        //         $(secondguide).hide(300);
+        //     } if($(thirdguide).css('display') == 'flex'){
+        //         $(firstguide).show(300);
+        //         $(thirdguide).hide(300);
+        //     }
+        //     // alert('left');
+        //     // if($(firstguide).css('display', 'flex')){
+        //     //     $(secondguide).show(300);
+        //     //     $(firstguide).hide(300);
+        //     // } else if($(secondguide).css('display', 'flex')){
+        //     //     $(thirdguide).show(300);
+        //     //     $(secondguide).hide(300);
+        //     // } else if($(thirdguide).css('display', 'flex')){
+        //     //     $(firstguide).show(300);
+        //     //     $(thirdguide).hide(300);
+        //     // }
+        //     // $(secondguide).show(300);
+        //     // $(firstguide).hide(300);
+        //     // $(thirdguide).hide(300);
+        //     // $(secondguide).css('display', 'flex');
+
+        // });
+        // $(left).click(function () {
+        //     if($(firstguide).css('display') == 'flex'){
+        //         $(thirdguide).show(300);
+        //         $(firstguide).hide(300);
+        //     } else if($(secondguide).css('display') == 'flex'){
+        //         $(firstguide).show(300);
+        //         $(secondguide).hide(300);
+        //     } else if($(thirdguide).css('display') == 'flex'){
+        //         $(secondguide).show(300);
+        //         $(thirdguide).hide(300);
+        //     }
+
+        // });
+    });
     let button = document.getElementById("clickblock");
     let navmenu = document.getElementById("b");
     let number = 1;
@@ -48,4 +139,13 @@ $(document).ready(function () {
             $(".burger__line_second").css('transform', 'rotate(0)');
         }
     })
+    $(document).ready(function () {
+        $("#menu").on("click", "a", function (event) {
+            event.preventDefault();
+            var id = $(this).attr('href'),
+                top = $(id).offset().top;
+            $('body,html').animate({ scrollTop: top }, 600);
+        });
+    });
+    
 })
